f648ae103a13ad241c65ef983ab9e8c57dcacbc9
Bruno Martins <bgcngm@gmail.com>
2018-07-10 00:18:09 +0100

    sdm845-common: Add a custom lights HAL
     * This overrides stock liblights and fixes LCD backlight issues.
    
    Change-Id: Iafa2ba2338932879b7a1dc480b50559e3328fd21

--- sm6150.mk
+++ sm6150.mk
@@ -251,6 +251,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_sdm845
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
