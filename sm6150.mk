7fabb7c93ae6bb4aa2c5c39623c1d9210e82fa1c
Daniel Rosenberg <drosen@google.com>
2021-07-16 12:52:43 +0200

    sm6150-common: Enable casefolding support on userdata
    This enables casefolding, which is required for removal of sdcardfs.
    
    Test: Wipe data and boot. lsattr /data/media, check for +F
    Bug: 138322712
    Change-Id: I020aa6bb9c121e7bda83c07bc9fb37dcfa26702c

--- sm6150.mk
+++ sm6150.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Viper
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
