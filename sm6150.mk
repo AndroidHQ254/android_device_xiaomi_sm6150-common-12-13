ac43844d3973ba899137f0b89457ac863eb2dff0
AndroidHQ254 <d.murimi14@gmail.com>
2021-11-22 18:45:29 +0300

    sm6150-common: Restore lineage livedisplay
    Restored from: https://github.com/AndroidHQ254/android_device_xiaomi_sm6150-common-12/commit/3831a16b5c9445f98a37d8c988d8dcfb4e3d1b93#diff-55caaa6a74a1a0e1869f98b7209a34f3a28b9ea025cce0ac17f6ad9c5072e8d4

--- sm6150.mk
+++ sm6150.mk
@@ -218,6 +218,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.xiaomi_sm6150
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
