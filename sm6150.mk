e3bae4b6e0489d16f08a407d65aca6ec009d5705
AndroidHQ254 <d.murimi14@gmail.com>
2022-01-04 16:31:05 +0300

    sm6150-common: Address duplicate derp

--- sm6150.mk
+++ sm6150.mk
@@ -79,7 +79,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     libbthost_if.vendor \
     libldacBT_bco.vendor
