d7b3b9dabb50e07414cbb72819c5108b30863d04
AndroidHQ254 <d.murimi14@gmail.com>
2022-01-06 13:25:16 +0300

    sm6150-common: Build missing light HAL service
    E servicemanager: Could not find android.hardware.light.ILights/default in the VINTF manifest.

--- sm6150.mk
+++ sm6150.mk
@@ -224,6 +224,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.xiaomi_sm6150
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
