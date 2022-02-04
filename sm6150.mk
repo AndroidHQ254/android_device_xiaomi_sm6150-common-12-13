83f4a0b60f4374060c352a3e18e2d78f6565fea4
Lucchetto <lucchetto.tie@live.com>
2022-02-04 20:11:19 +0300

    sm6150-common: Create dummy libqti-perfd-client
    Source: https://github.com/DerpFest-Devices/device_xiaomi_sm6150-common/tree/12/libqti-perfd-client
    
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    sm6150-common: libqti-perfd-client: Remove namespace declaration
    
    It's pointless when using extern "C".
    
    sm6150-common: libqti-perfd-client: Return a dummy value
    
    Return a positive integer for perf lock acquire and release so
    that Goodix/FPC fingerprint sensor blobs will not complain.
    
    Goodix:
    E [goodixHAL][gf_hal_milan_f_series]: goodix_perf_lock_acquire: Failed to acquire perf lock, err: 0
    E [goodixHAL][gf_hal_milan_f_series]: goodix_perf_lock_release: Perf lock release error 0
    
    FPC:
    E fpc_tac : fpc_perf_lock_acquire: Incorrect params, Failed to acquire perf lock, err
    E fpc_tac : fpc_perf_lock_release: Perf lock release error 0
    
    sm6150-common: libqti-perfd-client: Move to C
    
    * Why bothering with C++ mangling when we can just build it as a standard C library?

--- sm6150.mk
+++ sm6150.mk
@@ -290,6 +290,7 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock \
+    libqti-perfd-client \
     vendor.qti.hardware.perf@2.0.vendor
 
 # QTI
