904023d67b6798ec909bc90441fcebc30f8be1e9
Andrzej Perczak <linux@andrzejperczak.com>
2022-02-28 23:09:06 +0100

    sm6150-common: Remove wifi-aware support
    It errors out during boot despite fw reporting NaN support.
    
    Test: dumpsys wifiaware reports null provider
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: I148b06aafe167d2d787fa71bc50df7823020212b

--- sm6150.mk
+++ sm6150.mk
@@ -555,7 +555,6 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi)
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
