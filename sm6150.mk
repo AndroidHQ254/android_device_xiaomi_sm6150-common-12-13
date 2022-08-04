28b021e2e8771ff1fcb50b7eb1d289721f3e1097
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-04 23:21:53 +0300

    sm6150-common: Build Pixel thermal HIDL

--- sm6150.mk
+++ sm6150.mk
@@ -505,6 +505,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.pixel \
+    thermal_symlinks
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_sm6150
