0aa2268915ac1fbd1b46acc7a380036d5a622f00
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-09-21 16:31:50 +0300

    sm6150-common: Build libldacBT packages
    * Now opensource on A13

--- sm6150.mk
+++ sm6150.mk
@@ -87,7 +87,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     libbthost_if \
-    libldacBT_bco
+    libldacBT_bco \
+    libldacBT_enc \
+    libldacBT_abr
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
