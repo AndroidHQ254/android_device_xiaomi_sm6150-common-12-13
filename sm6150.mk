1948257437d85ae1ab5b9ec649cd9cae52b0f7f9
AndroidHQ254 <d.murimi14@gmail.com>
2022-09-21 16:38:30 +0300

    sm6150-common: Uprev android.hardware.bluetooth.audio & add to fcm

--- sm6150.mk
+++ sm6150.mk
@@ -82,6 +82,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     libldacBT_enc \
     libldacBT_abr \
