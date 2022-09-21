633c1716919e003f169723ec06ca3e37b2c815e3
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-09-21 16:31:50 +0300

    sm6150-common: Build libldacBT packages
    * Now opensource on A13

--- sm6150.mk
+++ sm6150.mk
@@ -89,6 +89,8 @@ PRODUCT_PACKAGES += \
     libbthost_if \
     libbthost_if.vendor \
     libldacBT_bco.vendor \
+    libldacBT_enc \
+    libldacBT_abr \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
