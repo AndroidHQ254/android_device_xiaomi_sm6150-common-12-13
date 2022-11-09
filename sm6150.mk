cf3744a2fe3a80b0232615e338d6aecd2fdac0e4
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-11-09 23:32:44 +0300

    sm6150-common: Address bluetooth packages derp

--- sm6150.mk
+++ sm6150.mk
@@ -82,16 +82,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     libbthost_if \
     libbthost_if.vendor \
     libldacBT_bco.vendor \
     libldacBT_enc \
-    libldacBT_abr
+    libldacBT_abr \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
