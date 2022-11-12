b3d7d952f14788bfcce283a21f658ea3f866d822
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-11-12 17:15:12 +0300

    [SQUASH] sm6150-common: parts: Drop support for Hi-Fi
    This reverts commit e543ad72afd8737ed43a5e4577b7210e316cee2d.
    
    Revert "sm6150-common: parts: dirac: Add hifi vector icons"
    
    This reverts commit 5c80d89cc0908c536ab22e3b1e9ebda69db2313b.
    
    Revert "sm6150-common: parts: Disable Hi-Fi preference when toggling switchbar"
    
    This reverts commit 119d60afb777003fd506df1e251b02934152084a.
    
    Revert "sm6150-common: parts: Fix "dirac_hifi_enable_title" string"
    
    This reverts commit dbc056678ebf03d8fcac3cdf23bc2c04d03c2c28.

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -23,7 +23,6 @@ import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
 
 import com.android.settingslib.widget.MainSwitchPreference;
 import com.android.settingslib.widget.OnMainSwitchChangeListener;
@@ -35,7 +34,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
 
     private static final String PREF_ENABLE = "dirac_enable";
     private static final String PREF_HEADSET = "dirac_headset_pref";
-    private static final String PREF_HIFI = "dirac_hifi_pref";
     private static final String PREF_PRESET = "dirac_preset_pref";
     
     public static final String PREF_SCENE = "scenario_selection";
@@ -44,7 +42,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
 
     private ListPreference mHeadsetType;
     private ListPreference mPreset;
-    private SwitchPreference mHifi;
     private ListPreference mScenes;
 
     @Override
@@ -60,17 +57,11 @@ public class DiracSettingsFragment extends PreferenceFragment implements
 
         mHeadsetType = (ListPreference) findPreference(PREF_HEADSET);
         mHeadsetType.setOnPreferenceChangeListener(this);
+        mHeadsetType.setEnabled(enhancerEnabled);
 
         mPreset = (ListPreference) findPreference(PREF_PRESET);
         mPreset.setOnPreferenceChangeListener(this);
-
-        mHifi = (SwitchPreference) findPreference(PREF_HIFI);
-        mHifi.setOnPreferenceChangeListener(this);
-
-        boolean hifiEnable = DiracUtils.getHifiMode();
-        mHeadsetType.setEnabled(!hifiEnable && enhancerEnabled);
-        mPreset.setEnabled(!hifiEnable && enhancerEnabled);
-        mHifi.setEnabled(enhancerEnabled);
+        mPreset.setEnabled(enhancerEnabled);
 
         mScenes = (ListPreference) findPreference(PREF_SCENE);
         mScenes.setOnPreferenceChangeListener(this);
@@ -82,13 +73,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         switch (preference.getKey()) {
             case PREF_HEADSET:
                 DiracUtils.setHeadsetType(Integer.parseInt(newValue.toString()));
-                return true;
-            case PREF_HIFI:
-                DiracUtils.setHifiMode((Boolean) newValue ? 1 : 0);
-                if (DiracUtils.isDiracEnabled()) {
-                    mHeadsetType.setEnabled(!(Boolean) newValue);
-                    mPreset.setEnabled(!(Boolean) newValue);
-                }
                 return true;
             case PREF_PRESET:
                 DiracUtils.setLevel((String) newValue);
@@ -106,14 +90,8 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mSwitchBar.setChecked(isChecked);
 
         DiracUtils.setEnabled(isChecked);
-        mHifi.setEnabled(isChecked);
         mHeadsetType.setEnabled(isChecked);
         mPreset.setEnabled(isChecked);
         mScenes.setEnabled(isChecked);
-
-        if (!isChecked){
-            mHifi.setChecked(false);
-            DiracUtils.setHifiMode(0);
-        }
     }
 }
