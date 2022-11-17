18f886e64a30271c50b444c262a0a28367506c9c
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-11-17 17:31:45 +0300

    sm6150-common: Include MiuiCamera sepolicy here

--- sm6150.mk
+++ sm6150.mk
@@ -106,7 +106,6 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
-    libxml2.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
