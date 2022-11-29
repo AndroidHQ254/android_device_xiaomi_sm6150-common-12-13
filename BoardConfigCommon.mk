6b2316a5659b9cdabb3424340b1b1e7e86669d79
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-11-29 19:45:11 +0300

    sm6150-common: Add KERNEL_LD kernel flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,7 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += swiotlb=1
+KERNEL_LD := LD=ld.lld
 
 # TARGET_KERNEL_APPEND_DTB handling
 ifeq ($(strip $(PRODUCT_USE_DYNAMIC_PARTITIONS)),true)
