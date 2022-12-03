92d2d7be313711bbea4994a7305f7a07517d6ebe
Erfan Abdi <erfangplus@gmail.com>
2022-12-04 01:41:47 +0300

    sm6150-common: sepolicy: Move wfd debug prop out of user build

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,6 +184,9 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor_test
+endif
 
 # Udfps
 ifeq ($(TARGET_HAS_UDFPS),true)
