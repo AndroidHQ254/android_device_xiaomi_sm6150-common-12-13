93a61456881fbaf58847884ad947ec11970f441f
Android HQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2022-12-05 04:16:53 +0300

    sm6150-common: Move to common Xiaomi displayfeature HIDL

--- sm6150.mk
+++ sm6150.mk
@@ -603,6 +603,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Xiaomi displayfeature
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.displayfeature@1.0.vendor
+
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
