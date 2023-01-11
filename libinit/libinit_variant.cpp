906c9cf70a1c99a1b2e7004802199916b23843eb
luk1337 <priv.luk@gmail.com>
2023-01-11 19:25:57 +0300

    sm6150-common: libinit: Don't override fingerprint in recovery
    - Incremental ota checks for build fingerprint

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 #include <libinit_utils.h>
 
@@ -34,7 +35,12 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("mod_device", variant.mod_device, true);
     set_ro_build_prop("model", variant.model, true);
 
-    property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
+    if (access("/system/bin/recovery", F_OK) != 0) {
+        set_ro_build_prop("fingerprint", variant.build_fingerprint);
+        property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
+
+        property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
+    }
 
     if (variant.nfc)
         property_override(SKU_PROP, "nfc");
