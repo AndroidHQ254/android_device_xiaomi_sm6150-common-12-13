ae841d4912decb7f76956bef62901483b67b7476
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-03-06 12:41:49 +0300

    sm6150-common: Build touchfeature HIDL lib from source

--- sm6150.mk
+++ sm6150.mk
@@ -517,6 +517,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_sm6150
 
+# Touchfeature
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.touchfeature@1.0.vendor
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
