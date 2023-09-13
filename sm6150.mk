4c7924f484f78a9e4b72c4273296a180a9754aae
AndroidHQ254 <14816738+AndroidHQ254@users.noreply.github.com>
2023-09-13 11:26:00 +0300

    Revert "davinci: Use 6GB dalvik-heap config"
    This reverts commit 454e90dd7b225967fa080d42b84420156a92b084.
    
    * Taken care of by libinit already

--- sm6150.mk
+++ sm6150.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 
