a743df7aa1f67591f6ee2ead8ba031727cf59cbe
adi8900 <adrianszymanski242@gmail.com>
2023-10-09 21:06:44 +0200

    sm6150-common: Build system vendor.qti.hardware.display.config-V5-ndk from source for wfd stack
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -625,7 +625,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData.system \
     libwfdaac_vendor \
     libwfdaac_proprietary \
-    vendor.display.config@2.0
+    vendor.display.config@2.0 \
+    vendor.qti.hardware.display.config-V5-ndk
 
 # WfdCommon
 PRODUCT_BOOT_JARS += \
