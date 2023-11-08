dbc04b11fc4ced54a1594596c1382e4e084a5e6e
AndroidHQ254 <d.murimi14@gmail.com>
2023-11-08 13:49:23 +0300

    sm6150-common: Drop non-existing config

--- sm6150.mk
+++ sm6150.mk
@@ -436,7 +436,6 @@ PRODUCT_PACKAGES += \
     CarrierConfigSM6150 \
     DialerSM6150 \
     FrameworksSM6150 \
-    LineageSDKSM6150 \
     SettingsLibSM6150 \
     SettingsProviderSM6150 \
     SettingsSM6150 \
